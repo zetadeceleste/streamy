e7908df210b5e643827c64c5428498350836b6d5
Celeste Zapata <czapata@vaypol.com.ar>
2020-05-23 11:35:01 -0300

    Init commit

---
+++ precache-manifest.56460a5696a9feede406d5c5483f85bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7411abf93dc0050ae114a0346783c2a",
+    "url": "/streams-client/index.html"
+  },
+  {
+    "revision": "12ef3a90f50ab0999eef",
+    "url": "/streams-client/static/js/2.5319c218.chunk.js"
+  },
+  {
+    "revision": "ce7acd6010885da06b5b258f71508140",
+    "url": "/streams-client/static/js/2.5319c218.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35cebc2171648e01d82e",
+    "url": "/streams-client/static/js/main.17822913.chunk.js"
+  },
+  {
+    "revision": "f2c9ee3d73ae549cbdae",
+    "url": "/streams-client/static/js/runtime-main.97e2d234.js"
+  }
+]);
